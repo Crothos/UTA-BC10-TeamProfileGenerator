55e9b98d2a0f33a5d7c5965d449468a12f923af6
Crothos <s.g.totten@gmail.com>
2023-01-27 21:57:47 -0600

    Generates Manager card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -8,9 +8,18 @@ function generateHTML(team) {
         <link rel="stylesheet" href="./dist/style.css">
     </head>
     <body>
-        <header id = "header">${team[0].name}'s Team</header>
-        <div id = "cardcontainer">
-            
+        <header id="header">My Team</header>
+        <div id="cardcontainer">
+        <div class="card">
+        <img class="card-img-top" src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-glasses-outline-512.png" alt="Card image cap" style="width:50px;height:50px" >
+        <div class="card-body">
+            <h2>Manager</h2>
+            <h3>Name: ${team[0].name}</h3>
+            <h3>Employee ID: ${team[0].id}
+            <h3>Email: <a href="mailto:${team[0].email}">${team[0].email}</a></h3>
+            <h3>Office Number: ${team[0].officeNumber}</h3>
+        </div>
+      </div>
         </div>
     </body>
     </html>
